42d2754dc6f16bd7b32da37f22d2bc61b9eade81
Mohammad Yousefi <vahid_you2004@yahoo.com>
2019-05-16 00:07:32 +0430

    Delete makedep.mk

--- Test Project/Release/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-..\ASK Remote Control Decoder\ASKRemoteControlDecoder.cpp
-
-ASKRmtCtrlDcdr.cpp
-
